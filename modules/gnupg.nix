85222875bcee31152dc5412441cb0991722ff86b
John A. Lotoski <johnalotoski@gmail.com>
2020-04-27 20:21:12 -0400

    Clean up the gpg related activation script

--- modules/gnupg.nix
+++ modules/gnupg.nix
@@ -3,13 +3,14 @@
   environment.systemPackages = with pkgs; [ gnupg pinentry gpgme.dev ];
   programs.gnupg.agent.enable = true;
 
+  environment.etc."per-user/root/gpg-agent.conf".text = "allow-loopback-pinentry";
+  environment.etc."per-user/root/gpg.conf".text = "pinentry-mode loopback";
+
   environment.etc."per-user/${secrets.priUsr}/gpg-agent.conf".text = "#pinentry-program /run/current-system/sw/bin/pinentry-curses";
   environment.etc."per-user/${secrets.priUsr}/gpg.conf".text = ''
     default-key ${secrets.gpg.fp.pri}
     #default-key ${secrets.gpg.fp.sec}
   '';
-  environment.etc."per-user/root/gpg-agent.conf".text = "allow-loopback-pinentry";
-  environment.etc."per-user/root/gpg.conf".text = "pinentry-mode loopback";
 
   system.activationScripts.gnupg = let
     gpgmejsonFile = pkgs.writeText "gpgmejson.json" ''
@@ -23,15 +24,23 @@
     '';
   in {
     text = ''
-      mkdir -p {/home/${secrets.priUsr},/root}/.gnupg ${secrets.priUsr}/.config/google-chrome/NativeMessagingHosts
-      [ -f /home/${secrets.priUsr}/.gnupg/gpg-agent.conf ] || cp /etc/per-user/${secrets.priUsr}/gpg-agent.conf /home/${secrets.priUsr}/.gnupg/gpg-agent.conf
-      [ -f /home/${secrets.priUsr}/.gnupg/gpg.conf ] || cp /etc/per-user/${secrets.priUsr}/gpg.conf /home/${secrets.priUsr}/.gnupg/gpg.conf
+      mkdir -p /root/.gnupg
       [ -f /root/.gnupg/gpg-agent.conf ] || cp /etc/per-user/root/gpg-agent.conf /root/.gnupg/gpg-agent.conf
       [ -f /root/.gnupg/gpg.conf ] || cp /etc/per-user/root/gpg.conf /root/.gnupg/gpg.conf
-      [ -f /home/${secrets.priUsr}/.config/google-chrome/NativeMessagingHosts/gpgmejson.json ] || \
+      chmod 644 /root/.gnupg/*.conf
+      chmod 0700 /root/.gnupg
+
+      mkdir -p /home/${secrets.priUsr}/.gnupg
+      [ -f /home/${secrets.priUsr}/.gnupg/gpg-agent.conf ] || cp /etc/per-user/${secrets.priUsr}/gpg-agent.conf /home/${secrets.priUsr}/.gnupg/gpg-agent.conf
+      [ -f /home/${secrets.priUsr}/.gnupg/gpg.conf ] || cp /etc/per-user/${secrets.priUsr}/gpg.conf /home/${secrets.priUsr}/.gnupg/gpg.conf
+      chown -R ${secrets.priUsr}:users /home/${secrets.priUsr}/.gnupg
+      chmod 644 /home/${secrets.priUsr}/.gnupg/*.conf
+      chmod 0700 /home/${secrets.priUsr}/.gnupg
+
+      mkdir -p /home/${secrets.priUsr}/.config/google-chrome/NativeMessagingHosts
+      [ -e /home/${secrets.priUsr}/.config/google-chrome/NativeMessagingHosts/gpgmejson.json ] || \
         ln -s ${gpgmejsonFile} /home/${secrets.priUsr}/.config/google-chrome/NativeMessagingHosts/gpgmejson.json
-      chown -R ${secrets.priUsr}:users /home/${secrets.priUsr}/{.gnupg,.config/google-chrome/NativeMessagingHosts}
-      chmod 644 {/home/${secrets.priUsr},/root}/.gnupg/*.conf
+      chown -R ${secrets.priUsr}:users /home/${secrets.priUsr}/.config/google-chrome
     '';
     deps = [];
   };
