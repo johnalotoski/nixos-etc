86f8736d5454f938166aa454bd52947f23c4817f
John A. Lotoski <johnalotoski@gmail.com>
2024-07-21 20:20:48 -0500

    change: power cfg for serval

--- hw/hw-serval.nix
+++ hw/hw-serval.nix
@@ -14,7 +14,7 @@
   boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "sdhci_pci"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
-  boot.kernelParams = ["intel_idle.max_cstate=1"];
+  # boot.kernelParams = ["intel_idle.max_cstate=1"];
   boot.extraModulePackages = [];
 
   boot.loader.systemd-boot.enable = true;
--- machines/machine-serval.nix
+++ machines/machine-serval.nix
@@ -8,7 +8,7 @@
   imports = [
     ../hw/hw-serval.nix
     # ../modules/backup.nix
-    # ../modules/cardano-node.nix
+    ../modules/cardano-node.nix
     # ../modules/cuda.nix
     ../modules/db.nix
     # ../modules/fax.nix
@@ -35,6 +35,76 @@
     ../modules/yubikey.nix
     ../modules/zfs.nix
   ];
+
+  # services.power-profiles-daemon.enable = false;
+  # services.auto-cpufreq.enable = true;
+  # services.auto-cpufreq.settings = {
+  #   battery = {
+  #     governor = "powersave";
+  #     turbo = "never";
+  #   };
+  #   charger = {
+  #     # governor = "performance";
+  #     # turbo = "auto";
+  #     governor = "powersave";
+  #     turbo = "never";
+  #   };
+  # };
+
+  ### KERNEL
+  # boot.kernelParams = [
+  #   "ahci.mobile_lpm_policy=3"
+  #   "rtc_cmos.use_acpi_alarm=1"
+  # ];
+
+  # ### HWP
+  # systemd.tmpfiles.rules = [
+  #   "w /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference - - - - balance_power"
+  # ];
+
+  # ### TLP
+  # services.tlp = {
+  #   enable = true;
+  #   settings = {
+  #     # CPU_SCALING_GOVERNOR_ON_AC = "performance";
+  #     CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+  #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+  #     # CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+  #     CPU_ENERGY_PERF_POLICY_ON_AC = "power";
+  #     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+  #     # PLATFORM_PROFILE_ON_AC = "performance";
+  #     PLATFORM_PROFILE_ON_AC = "low_power";
+  #     PLATFORM_PROFILE_ON_BAT = "low-power";
+
+  #     # CPU_BOOST_ON_AC = 1;
+  #     CPU_BOOST_ON_AC = 0;
+  #     CPU_BOOST_ON_BAT = 0;
+
+  #     # CPU_HWP_DYN_BOOST_ON_AC = 1;
+  #     CPU_HWP_DYN_BOOST_ON_AC = 0;
+  #     CPU_HWP_DYN_BOOST_ON_BAT = 0;
+
+  #     #CPU_MIN_PERF_ON_AC = 0;
+  #     #CPU_MAX_PERF_ON_AC = 100;
+  #     #CPU_MIN_PERF_ON_BAT = 0;
+  #     #CPU_MAX_PERF_ON_BAT = 20;
+
+  #     #Optional helps save long term battery health
+  #     START_CHARGE_THRESH_BAT0 = 60; # 60 and below it starts to charge
+  #     STOP_CHARGE_THRESH_BAT0 = 90; # 90 and above it stops charging
+  #   };
+  # };
+
+  # ### SYSTEM 76 SCHEDULER
+  # services.system76-scheduler.settings.cfsProfiles.enable = true;
+
+  # ### POWERTOP
+  # #powerManagement.powertop.enable = true;
+
+  # ### ThermalD
+  # services.thermald.enable = true;
 
   networking.nat = {
     enable = true;
