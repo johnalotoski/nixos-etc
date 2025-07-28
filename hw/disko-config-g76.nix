396b9f1fd279ff6b3ce9862ec0fcb7097a07bad3
John A. Lotoski <johnalotoski@gmail.com>
2025-07-27 20:31:51 -0500

    disko: add g76

---
+++ hw/disko-config-g76.nix
@@ -0,0 +1,134 @@
+{disks ? ["/dev/nvme0n1" "/dev/nvme1n1"], ...}: {
+  disko.devices = {
+    disk = {
+      x = {
+        type = "disk";
+        device = builtins.elemAt disks 0;
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "ESP";
+              type = "ef00";
+              start = "0%";
+              end = "1GiB";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
+              };
+            }
+            {
+              name = "zfs";
+              type = "bf01";
+              start = "1GiB";
+              end = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            }
+          ];
+        };
+      };
+      y = {
+        type = "disk";
+        device = builtins.elemAt disks 1;
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "RECOVERY";
+              start = "0%";
+              end = "1GiB";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/recovery";
+                mountOptions = ["umask=0077"];
+              };
+            }
+            {
+              name = "zfs";
+              start = "1GiB";
+              end = "100%";
+              content = {
+                type = "zfs";
+                pool = "storage";
+              };
+            }
+          ];
+        };
+      };
+    };
+    zpool = {
+      zroot = {
+        type = "zpool";
+        rootFsOptions = {
+          mountpoint = "none";
+          compression = "zstd";
+          "com.sun:auto-snapshot" = "false";
+          acltype = "posixacl";
+          xattr = "sa";
+          encryption = "aes-256-gcm";
+          keyformat = "passphrase";
+          keylocation = "prompt";
+        };
+        options = {
+          ashift = "12";
+          autotrim = "on";
+        };
+        datasets = {
+          root = {
+            type = "zfs_fs";
+            mountpoint = "/";
+          };
+
+          nix = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+            options = {
+              atime = "off";
+            };
+          };
+
+          home = {
+            type = "zfs_fs";
+            mountpoint = "/home";
+          };
+
+          var = {
+            type = "zfs_fs";
+            mountpoint = "/var";
+          };
+
+          "var/log" = {
+            type = "zfs_fs";
+            mountpoint = "/var/log";
+          };
+        };
+      };
+
+      storage = {
+        type = "zpool";
+        rootFsOptions = {
+          mountpoint = "/storage";
+          compression = "zstd";
+          "com.sun:auto-snapshot" = "false";
+          acltype = "posixacl";
+          xattr = "sa";
+          encryption = "aes-256-gcm";
+          keyformat = "passphrase";
+          keylocation = "prompt";
+        };
+        options = {
+          ashift = "12";
+          autotrim = "on";
+        };
+      };
+    };
+  };
+}
