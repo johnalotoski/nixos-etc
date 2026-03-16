6ca64b91e08cf5f1029197acfed9157302c6424e
John A. Lotoski <johnalotoski@gmail.com>
2026-03-16 16:48:38 -0500

    cleanup: rm old secrets handling

--- modules/wireguard.nix
+++
@@ -1,6 +0,0 @@
-{config, ...}: let
-  hostName = config.networking.hostName;
-in {
-  networking.wireguard.interfaces = secrets."${hostName}".wireguard.interfaces;
-  networking.firewall.allowedUDPPorts = secrets."${hostName}".wireguard.listenPorts;
-}
